f4daf96a1c25ef318ba3df2168bbc398074f0c0b
Thomas Eby <tgeby@icloud.com>
2025-09-23 13:02:44 -0400

    Added mock for the window.matchMedia function

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom';
+
+import { beforeAll } from 'vitest';
+
+beforeAll(() => {
+  // Mocking window.matchMedia
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // deprecated
+        removeListener: vi.fn(), // deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+  });
+});
